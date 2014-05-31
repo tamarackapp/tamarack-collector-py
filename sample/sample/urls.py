10e040890f5dd1297aa4fa04ce2cf9e9db447d09
mhallin <mhallin@gmail.com>
2014-05-31 13:56:54 +0200

    Use a generic TemplateView for the sample's about page

--- sample/sample/urls.py
+++ sample/sample/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    url(r'^$', 'mini.views.index'),
-    url(r'^about$', 'mini.views.about'),
+    url(r'^$', 'mini.views.index', name='index'),
+    url(r'^about$', TemplateView.as_view(template_name='about.html'), name='about'),
     # Examples:
     # url(r'^$', 'sample.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
